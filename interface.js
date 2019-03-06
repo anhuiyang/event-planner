41abc99f2f88b63978009320e681a94c1631eb91
Hui An Yang <hecatie36@hotmail.com>
2019-03-06 11:35:01 +0000

    interface + sort of feature test failed

--- interface.js
+++ interface.js
@@ -1,12 +1,16 @@
 'use strict'
-
-// function loadPage(){
-//     let timeInput = document.getElementById("timeINput").value
-//     let eventInput = document.getElementById("eventInput").value
-//     let sendButton = document.getElementById("sendButton")
-//     sendButton.onclick = function(){
-
-//     }
-
-// }
-// document.onload = loadPage()
+let eventList = new evenList
+function loadPage(){
+    let timeInput = document.getElementById("timeINput").value
+    let eventInput = document.getElementById("eventInput").value
+    let sendButton = document.getElementById("sendButton")
+    let eventsOnPage = document.getElementById("eventsOnPage")
+    sendButton.onclick = function(){
+        let newEvent = new Event(timeInput, eventInput)
+        eventList.push(newEvent)
+    }
+    eventsOnPage = function(evenList){
+        
+    }
+}
+document.onload = loadPage()
