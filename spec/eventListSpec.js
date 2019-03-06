6c519be4f9a31e11c9fe4bb1ddb52c3353835c8a
Hui An Yang <hecatie36@hotmail.com>
2019-03-06 12:50:29 +0000

    unit test of eventList function filter failed

--- spec/eventListSpec.js
+++ spec/eventListSpec.js
@@ -5,13 +5,17 @@ describe("eventList", function(){
     beforeEach(function(){
         eventList = new EventList()
         event1 = {dateTime: "2019-03-06T17:00", description: "Group retro"}
+        event2 = {dateTime: "2019-03-01T17:00", description: "Passed event"}
+
     })
     it("can add new event to the list", function(){
         eventList.add(event1)
         expect(eventList.all.length).toBe(1)
     })
-    it("will show all event's", function(){
+    it("will filter out passed events", function(){
         eventList.add(event1)
-        expect(eventList.show).toBe([event1])
+        eventList.add(event2)
+        eventList.filter()
+        expect(eventList.orderedComingEvents.length).toBe(1)
     })
 })
--- src/eventList.js
+++ src/eventList.js
@@ -2,6 +2,7 @@
 class EventList{
     constructor() {
         this.all = []
+        this.orderedComingEvents = []
     }
     add(newEvent){
         this.all.push(newEvent)
