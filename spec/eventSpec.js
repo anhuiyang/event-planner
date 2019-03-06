37692dc88df0f00c843966af9a51f2ce7b5be11c
Hui An Yang <hecatie36@hotmail.com>
2019-03-06 11:47:16 +0000

    unit test of initialize event class passed

--- spec/eventSpec.js
+++ spec/eventSpec.js
@@ -4,7 +4,7 @@ describe("Event", function(){
         event = new Event("2019-03-06T17:00", "Group retro")
     })
     it("will save date & time", function(){
-        expect(event.dateTime).toBe("2019-03-06T11:40")
+        expect(event.dateTime).toBe("2019-03-06T17:00")
     })
     it("will save event description", function(){
         expect(event.description).toBe("Group retro")
---
+++ src/event.js
@@ -0,0 +1,7 @@
+class Event{
+    constructor(dateTime, description){
+        this.dateTime = dateTime
+        this.description = description
+    }
+    
+}
